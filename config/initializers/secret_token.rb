cb5e7fdcb7b30f20f8815d62bfcdbdd8d32ed5a7
Stephen Lu <munk801@gmail.com>
2013-12-25 14:44:30 -0600

    Initial Rails commit for hyl

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hyl::Application.config.secret_key_base = '3a05e629ad35458c8ca5d561a9c8d08b8c1d4b2af6cde93f9d7324331a224d542052dd7967ee08400b67dd352f8b85974b4996d56656fd9c9864d33c722aa8a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hyl::Application.config.session_store :cookie_store, key: '_hyl_session'
